508f094a2c03d02dd58b16843d4f52bbb97b43a6
dam2k <alex09.ar19@gmail.com>
2021-12-02 17:17:18 +0100

    Comienzo Practica5

---
+++ app/src/main/java/practica5/AlexandroRodriguez/iesseveroochoa/net/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package practica5.AlexandroRodriguez.iesseveroochoa.net.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import practica5.AlexandroRodriguez.iesseveroochoa.net.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
