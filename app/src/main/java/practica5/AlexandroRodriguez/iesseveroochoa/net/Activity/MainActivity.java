e24b5394886f84f28cd9b636949b5dda6d7daaab
dam2k <alex09.ar19@gmail.com>
2021-12-09 19:00:57 +0100

    Empezando con la clase DiaDiario

--- app/src/main/java/practica5/AlexandroRodriguez/iesseveroochoa/net/Activity/MainActivity.java
+++ app/src/main/java/practica5/AlexandroRodriguez/iesseveroochoa/net/Activity/MainActivity.java
@@ -3,6 +3,7 @@ package practica5.AlexandroRodriguez.iesseveroochoa.net.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import practica5.AlexandroRodriguez.iesseveroochoa.net.R;
 
@@ -13,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setSupportActionBar(findViewById(R.id.toolbar));
+
+
+    }
+
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
